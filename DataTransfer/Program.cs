37b8ebad102f9dfd9c4e5521e1fd17f6c7af0571
Rami Saidov <RamiS@bezeqint.co.il>
2018-07-30 21:14:11 +0300

    added entity framework and started transferring data.

--- DataTransfer/Program.cs
+++ DataTransfer/Program.cs
@@ -1,6 +1,6 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Data.OleDb;
-using MySql.Data.MySqlClient;
 
 namespace DataTransfer
 {
@@ -8,44 +8,102 @@ namespace DataTransfer
     {
         private static void Main(string[] args)
         {
-            using (var accessConnection = ConnectToAccess())
-            using (var mysqlConnection = ConnectToMySql())
+            using (OleDbConnection accessConnection = ConnectToAccess())
+            using (ClientConnectModelContainer modelContainer = new ClientConnectModelContainer())
             {
-                TransferData(accessConnection, mysqlConnection);
+                TransferData(accessConnection, modelContainer);
             }
         }
 
-        private static void TransferData(OleDbConnection accessConnection, MySqlConnection mysqlConnection)
+        private static void TransferData(OleDbConnection accessConnection, ClientConnectModelContainer modelContainer)
         {
             using (OleDbCommand oleDbCommand = new OleDbCommand("select * from Switch_DB", accessConnection))
             {
-                var reader = oleDbCommand.ExecuteReader();
+                OleDbDataReader reader = oleDbCommand.ExecuteReader();
                 while (reader.Read())
                 {
+                    // create switch
+                    Switch sw = new Switch();
+                    sw.Id = int.Parse(reader.GetString(reader.GetOrdinal("SwitchID")));
+                    sw.CrmNum = null;
+                    sw.Comments = reader["Comment"] as string;
+                    sw.Name = reader.GetString(reader.GetOrdinal("Switch_Name"));
+                    sw.SwRelease = reader.GetString(reader.GetOrdinal("SW_Release"));
+                    sw.MachineType =
+                        GetMachineTypeComment(reader.GetString(reader.GetOrdinal("Machine")), accessConnection);
+                    int? customerId = reader["CustomerID"] as int?;
+                    sw.SiteId = customerId != null && customerId == 0 ? null : customerId;
+                    sw.Tid = reader.GetString(reader.GetOrdinal("TID"));
+                    modelContainer.SwitchSet.Add(sw);
+                    modelContainer.SaveChanges();
 
+                    Console.WriteLine("added switch id = {0}", sw.Id);
+
+                    // create pbx connection
+                    string dialNum = reader.GetString(reader.GetOrdinal("DialNum"));
+                    if (!string.IsNullOrWhiteSpace(dialNum))
+                    {
+                        PbxConnection pbxConnection = new PbxConnection();
+                        pbxConnection.SwitchId = sw.Id;
+                        pbxConnection.DialNum = reader.GetString(reader.GetOrdinal("Area")) + dialNum;
+                        pbxConnection.LoginName = reader.GetString(reader.GetOrdinal("UserMerk"));
+                        pbxConnection.LoginPassword = reader.GetString(reader.GetOrdinal("Password"));
+                        pbxConnection.DebugPassword = reader.GetString(reader.GetOrdinal("DebugPass"));
+                        pbxConnection.BaudRate = GetBaudRate(reader.GetString(reader.GetOrdinal("Baudrate")),
+                            accessConnection);
+                        pbxConnection.ParDataStop = GetParDataStop(reader.GetString(reader.GetOrdinal("Parity")),
+                            accessConnection);
+                        modelContainer.PbxConnectionSet.Add(pbxConnection);
+                        modelContainer.SaveChanges();
+
+                        Console.WriteLine("added pbx connection for switch id = {0}", sw.Id);
+                    }
+
+                    // create kolan connection
+
+                    // create telnet connection
+
+                    // add files
                 }
             }
         }
 
-        private static MySqlConnection ConnectToMySql()
+        private static string GetParDataStop(string parityId, OleDbConnection connection)
         {
-            var conn = new MySqlConnection
+            using (OleDbCommand oleDbCommand =
+                new OleDbCommand("select ParDatStop from ParityDB where ParityKod = @id", connection))
             {
-                ConnectionString = "server=127.0.0.1;uid=" + ConfigurationManager.AppSettings["mysql_username"] +
-                                   ";pwd=" + ConfigurationManager.AppSettings["mysql_password"] + ";database=" +
-                                   ConfigurationManager.AppSettings["mysql_schema"]
-            };
-            conn.Open();
+                oleDbCommand.Parameters.AddWithValue("@id", parityId);
+                return (string) oleDbCommand.ExecuteScalar();
+            }
+        }
 
-            return conn;
+        private static int GetBaudRate(string baudRateId, OleDbConnection connection)
+        {
+            using (OleDbCommand oleDbCommand =
+                new OleDbCommand("select Baudr from Baudrate_DB where BaudKod = @id", connection))
+            {
+                oleDbCommand.Parameters.AddWithValue("@id", baudRateId);
+                return int.Parse((string) oleDbCommand.ExecuteScalar());
+            }
+        }
+
+        private static string GetMachineTypeComment(string machineId, OleDbConnection connection)
+        {
+            using (OleDbCommand oleDbCommand =
+                new OleDbCommand("select Comment from MachineDB where Machine = @id", connection))
+            {
+                oleDbCommand.Parameters.AddWithValue("@id", machineId);
+                return (string) oleDbCommand.ExecuteScalar();
+            }
         }
 
         private static OleDbConnection ConnectToAccess()
         {
-            var connStr = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" +
-                          ConfigurationManager.AppSettings["access_db_filename"] + "; Jet OLEDB:Database Password=" +
-                          ConfigurationManager.AppSettings["access_db_password"];
-            var accessConnection = new OleDbConnection(connStr);
+            string connStr = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" +
+                             ConfigurationManager.AppSettings["access_db_filename"] + "; Jet OLEDB:Database Password=" +
+                             ConfigurationManager.AppSettings["access_db_password"];
+            OleDbConnection accessConnection = new OleDbConnection(connStr);
             accessConnection.Open();
             return accessConnection;
         }
